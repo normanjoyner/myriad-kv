adc5b33ca14a959fe954ce34a271185d9b502f8f
Norman Joyner <norman.joyner@gmail.com>
2016-04-09 01:11:19 -0400

    switch msgpack library

--- persistence/disk.js
+++ persistence/disk.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var _ = require("lodash");
-var message_pack = require("msgpack");
+var msgpack = require("msgpack-js");
 
 function Disk(options){
     _.defaults(options, {
@@ -28,12 +28,12 @@ Disk.prototype.bootstrap = function(fn){
         else if(_.isEmpty(data.toString()))
             return fn(null, {});
         else
-            return fn(null, message_pack.unpack(data));
+            return fn(null, msgpack.decode(data));
     });
 }
 
 Disk.prototype.snapshot = function(data, fn){
-    fs.writeFile([this.options.data_directory, this.options.snapshot_name].join("/"), message_pack.pack(data), fn);
+    fs.writeFile([this.options.data_directory, this.options.snapshot_name].join("/"), msgpack.encode(data), fn);
 }
 
 module.exports = Disk;
